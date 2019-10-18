1d4bc5d7fce667021d2d38f84c0633d028b22bbf
Prateeba <Prateeba.Ruggoo@ulb.ac.be>
2019-10-18 15:12:55 +0200

    [Parameter fitting]

--- Code/R/test_nprot.R
+++ Code/R/test_nprot.R
@@ -14,8 +14,8 @@ df=read.table("test2.dat")
 names(df)=c("time", "Ab42_rep1_5uM") 
 
 # plot data
-tmp=melt(df,id.vars=c("time"),variable.name="species",value.name="concentration")
-ggplot(data=tmp,aes(x=time,y=concentration,color=species))+geom_point(size=3)+geom_blank()
+#tmp=melt(df,id.vars=c("time"),variable.name="species",value.name="conc")
+#ggplot(data=tmp,aes(x=time,y=conc,color=species))+geom_point(size=3)+geom_blank()
 
 rxnrate=function(t,c,parms){
  	# rate constant passed through a list called parms
@@ -47,7 +47,7 @@ ssq=function(parms){
 	cinit=c(mt=1e-6,Mt=0,Pt=0)
 	# time points for which conc is reported
 	# include the points where data is available
-	t=c(seq(0,20,0.1),df$time)
+	t=c(seq(0,20,0.01),df$time)
 	t=sort(unique(t))
 	
 	# parameters from the parameter estimation routine
@@ -58,7 +58,7 @@ ssq=function(parms){
 	kplus=parms[5]
 	
 	# solve ODE for a given set of parameters
-	out=ode(y=cinit,times=t,func=rxnrate,parms=parms)
+	out=ode(y=cinit,times=t,func=rxnrate,parms=parms, method = "rk4")
 	
 	# Filter data that contains time points where data is available
 	outdf=data.frame(out)
@@ -71,10 +71,30 @@ ssq=function(parms){
 
 	# return predicted vs experimental residual
 	return(ssqres)
-
 }
 
 parms=list(kn=1,nc=1,k2=1,n2=1,kplus=1)
-fitval=nls.lm(par=parms,fn=ssq)
+fitval=nls.lm(par=parms,fn=ssq,lower=c(0, 0, 0, 0, 0),upper=c(Inf, 5, Inf, 5, Inf),control=nls.lm.control(maxiter=1000))
+
+# Estimated parameter
 parest=as.list(coef(fitval))
-parest
+
+# simulated predicted profile at estimated parameter values
+cinit=c(mt=1e-6,Mt=0,Pt=0)	
+t=c(seq(0,20,0.01),df$time)
+t=sort(unique(t))
+parms=as.list(parest)
+
+out=ode(y=cinit,times=t,func=rxnrate,parms=parms, method="rk4")
+outdf=data.frame(out)
+names(outdf)=c("time","Ab42_rep1_5uM_pred")
+
+# Overlay predicted profile with experimental data
+tmppred=melt(outdf,id.var=c("time"),variable.name="species",value.name="conc")
+tmpexp=melt(df,id.var=c("time"),variable.name="species",value.name="conc")
+p=ggplot(data=tmppred,aes(x=time,y=conc,color=species,linetype=species))+geom_line()
+p=p+geom_line(data=tmpexp,aes(x=time,y=conc,color=species,linetype=species))
+p=p+geom_point(data=tmpexp,aes(x=time,y=conc,color=species))
+p=p+scale_linetype_manual(values=c(0,1,0,1,0,1))
+p=p+scale_color_manual(values=rep(c("red","blue","green"),each=2))+theme_bw()
+print(p)
